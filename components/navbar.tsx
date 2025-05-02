01baa2887c820b33fdc09d82bfcea26adb31b06d
K.T.G.S. Sithumi <IT22903006@my.sliit.lk>
2025-05-02 16:12:47 +0530

    Fixing Navbar Section

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -48,6 +48,7 @@ export default function Navbar() {
       : []),
   ]
 
+
   return (
     <header className="sticky mx-auto top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between">
@@ -101,6 +102,7 @@ export default function Navbar() {
         </div>
 
         <div className="flex items-center gap-2">
+          
           {isLoggedIn ? (
             <>
               <NotificationDropdown />
